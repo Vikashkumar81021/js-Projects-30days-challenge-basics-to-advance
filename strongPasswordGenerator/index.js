86e1e3d561c1680d6faca435a0187c72fbc58cd7
vikash kumar <viku81021@gmail.com>
2024-02-18 17:58:44 +0530

    edit code in strongPPassword

--- strongPasswordGenerator/index.js
+++ strongPasswordGenerator/index.js
@@ -1,33 +1,9 @@
-// const InputVal = document.getElementById("password");
-// const outputVal = document.getElementById("output");
 
-// InputVal.addEventListener("input", () => {
-//   console.log(InputVal.value);
-//   let password = InputVal.value;
-//   if (password.length < 8) {
-//     outputVal.innerHTML = "password is short";
-//     outputVal.style.color="red"
-//   }
-//   if (password.search(/[a-z]/) == -1) {
-//     outputVal.innerHTML = "password is missing lowerCase";
-//     outputVal.style.color = "red";
-//   } else if (password.search(/[A-Z]/) == -1) {
-//     outputVal.innerHTML = "Password is missing UpparCase";
-//     outputVal.style.color = "red";
-//   }else if (password.search(/[0-9]/) == -1) {
-//     outputVal.innerHTML = "Password is numeric letter";
-//     outputVal.style.color = "red";
-//   }
-//    else {
-//     outputVal.innerHTML = "Password is strong";
-//     outputVal.style.color = "green";
-//   }
-// });
 const InputVal = document.getElementById("password");
 const outputVal = document.getElementById("output");
 
 InputVal.addEventListener("input", () => {
-  console.log(InputVal.value);
+
   let password = InputVal.value;
   
   if (password.length < 8) {
